e50c003ab0cac479c19f85ee6f734e5b59c0403c
kenaragon <kenny@longsight.com>
2018-06-07 08:57:03 -0700

    WFU Customization - ARES Configuration in BasicLTI
    WFU Customization - ARES Configuration in BasicLTI ; changing M_log variable

--- basiclti/basiclti-common/src/java/org/sakaiproject/basiclti/util/SakaiBLTIUtil.java
+++ basiclti/basiclti-common/src/java/org/sakaiproject/basiclti/util/SakaiBLTIUtil.java
@@ -27,7 +27,7 @@ import java.util.List;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.Enumeration;
-
+import java.lang.reflect.Method;
 import java.net.URL;
 import java.net.URLEncoder;
 
@@ -160,6 +160,15 @@ public class SakaiBLTIUtil {
 	public static final String CANVAS_PLACEMENTS_ASSIGNMENTSELECTION = "Canvas.placements.assignmentSelection";
 	public static final String CANVAS_PLACEMENTS_LINKSELECTION = "Canvas.placements.linkSelection";
 	public static final String CANVAS_PLACEMENTS_CONTENTIMPORT = "Canvas.placements.contentImport";
+	
+  private static Object inotadoService = null;
+	
+	private static Object getInotadoService(){
+		if(inotadoService == null){
+			inotadoService = ComponentManager.get("edu.wfu.inotado.api.InotadoService");
+		}
+		return inotadoService;
+	}
 
 	// Retrieve the property from the configuration unless it
 	// is overridden by the server configurtation (i.e. sakai.properties)
@@ -689,6 +698,22 @@ public class SakaiBLTIUtil {
 		// Send along the content link
 		String contentlink = toNull(getCorrectProperty(config,"contentlink", placement));
 		if ( contentlink != null ) setProperty(props,"ext_resource_link_content",contentlink);
+
+		// Send additional parameter section_id
+		if (getInotadoService() != null) {
+			try {
+				Method method;
+				method = inotadoService.getClass().getMethod("getSectionId",String.class);
+				String section_id = (String) method.invoke(inotadoService,
+						props.getProperty(BasicLTIConstants.CONTEXT_ID));
+				setProperty(props, "section_id", section_id);
+			} catch (Exception e) {
+				log.error("Failed to execute method due to error " + e.getMessage(), e);
+			}
+		} else {
+			log.warn("Unable to find the bean: inotadoService.");
+		}
+		
 	} 
 
 	public static void addGlobalData(Site site, Properties props, Properties custom, ResourceLoader rb)
