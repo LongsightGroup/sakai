0c95d25dfe3fa25cd67c7298051e26e9e35fa3ef
kenaragon <kenny@longsight.com>
2019-05-23 12:29:15 -0700

    61239 allegheny SAML

--- login/login-tool/tool/src/java/org/sakaiproject/login/filter/EppnSamlFilter.java
+++ login/login-tool/tool/src/java/org/sakaiproject/login/filter/EppnSamlFilter.java
@@ -15,6 +15,7 @@
  */
 package org.sakaiproject.login.filter;
 
+import org.apache.commons.lang.StringUtils;
 import org.opensaml.saml2.core.Attribute;
 import org.opensaml.xml.XMLObject;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -24,7 +25,7 @@ import org.springframework.security.saml.userdetails.SAMLUserDetailsService;
 public class EppnSamlFilter implements SAMLUserDetailsService {
         @Override
         public Object loadUserBySAML(SAMLCredential cred) throws UsernameNotFoundException {
-                // https://www.incommon.org/federation/attributesummary.html
-                return cred.getAttributeAsString("urn:oid:1.3.6.1.4.1.5923.1.1.1.6");
+                String userID = cred.getNameID().getValue();
+                return StringUtils.lowerCase(StringUtils.substringBefore(userID, "@"));
         }
 }
