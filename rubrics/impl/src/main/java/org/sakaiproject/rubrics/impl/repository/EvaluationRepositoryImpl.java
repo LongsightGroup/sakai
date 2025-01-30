1a740d18250248554559ed7ef11c4dbebcdb84aa
bgarciaentornos <bgarcia@entornosdeformacion.com>
2025-01-30 22:09:50 +0100

    SAK-50923 Rubrics prevent a self graded rubric from appearing as a regular rubric evaluation (#13255)
    (cherry picked from commit 1a9ed8866c3b572ea771197b040093f9fd180ca0)

--- rubrics/impl/src/main/java/org/sakaiproject/rubrics/impl/repository/EvaluationRepositoryImpl.java
+++ rubrics/impl/src/main/java/org/sakaiproject/rubrics/impl/repository/EvaluationRepositoryImpl.java
@@ -74,7 +74,8 @@ public class EvaluationRepositoryImpl extends SpringCrudRepositoryImpl<Evaluatio
         Root<Evaluation> eval = query.from(Evaluation.class);
         //Join<Evaluation, ToolItemRubricAssociation> ass = eval.join("association");
         query.where(cb.and(cb.equal(eval.get("associationId"), associationId),
-                            cb.equal(eval.get("evaluatedItemOwnerId"), userId)));
+                            cb.equal(eval.get("evaluatedItemOwnerId"), userId),
+                            cb.notEqual(eval.get("evaluatedItemId"), userId)));// SAK-50923 this is to prevent a self graded rubric from appearing as a regular rubric evaluation
 
         return session.createQuery(query).uniqueResultOptional();
     }
