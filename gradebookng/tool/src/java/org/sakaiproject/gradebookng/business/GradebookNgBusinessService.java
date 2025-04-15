4b44302e59ec9778a061db30d170442c54f025ed
Jesús María Méndez Pérez <jesus.mendez@ticarum.es>
2025-04-15 11:32:32 +0200

    SAK-51377 Gradebook: Globe icon is missing when sending gradebook item from external tool (#13610)
    (cherry picked from commit 59eca5746dd8a92a32c1239d2dc68ded4e18bb99)
    (cherry picked from commit 1621c2c416cdf2ce04085fe27ae0750049ca8a55)

--- gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/GradebookNgBusinessService.java
+++ gradebookng/tool/src/java/org/sakaiproject/gradebookng/business/GradebookNgBusinessService.java
@@ -3058,7 +3058,7 @@ public class GradebookNgBusinessService {
 	}
 
 	public String getDefaultIconClass() {
-		return ICON_SAKAI + "default-tool fa fa-globe";
+		return ICON_SAKAI + "default-tool bi bi-globe-americas";
 	}
 
 	private String getAssignmentsIconClass() {
